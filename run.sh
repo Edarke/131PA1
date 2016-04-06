a6e64a324e3581051781ee539e5f21ae8069cafc
Evan Darke <edarke@ucsd.edu>
2016-04-06 15:31:47 -0700

    updated run script

--- run.sh
+++ run.sh
@@ -4,6 +4,10 @@ RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m' # No Color
 
+dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+cd $dir
+
 for f in *.glsl; do
     ../glc < $f &> .temp
     printf "Comparing output with %s: " ${f%%.*}.out 
